eac194dfa10b7849083e1e11bcb6b57573b2490a
autumnjava <creizy@gmail.com>
2020-10-03 11:25:22 +0200

    updated fields in Player

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -3,7 +3,8 @@ package com.company;
 import java.util.ArrayList;
 
 public class Player {
-    private String[] names = {"Player 1", "Player 2", "Player 3", "Player 4"}; // default names of players
+
+    private String name;
     private int moneyBalance = 15000; //start summa
     private ArrayList<Dragon> dragonsOwned = new ArrayList<>();
     private Game game;

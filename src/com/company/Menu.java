5daf4eae450490a075a27e23f32b1de80d77aa82
autumnjava <creizy@gmail.com>
2020-10-03 11:02:07 +0200

    Added a Scanner field into Menu class. Created two static methods: print and prompt

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -3,5 +3,17 @@ package com.company;
 import java.util.Scanner;
 
 public class Menu {
-    Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
+
+    static public void print(String x){
+        // print a string if it is not empty
+        if(!x.equals("")){ System.out.println(x); }
+    }
+
+    static public String prompt(String message, String ..._default){
+        // prompt the user for an answer - use default answer (if such) if none
+        print(message);
+        var answer = scanner.nextLine().trim(); // trim removes spaces at start & end
+        return answer.equals("") && _default.length > 0 ? _default[0] : answer;
+    }
 }

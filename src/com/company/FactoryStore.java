92d57917a3a4f8b8974d1d5858c749bf488bf546
autumnjava <creizy@gmail.com>
2020-10-03 10:56:16 +0200

    Starting to create fields in FactoryStore class

--- src/com/company/FactoryStore.java
+++ src/com/company/FactoryStore.java
@@ -1,4 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class FactoryStore {
+    public String nameOfStore = "Dragons and Food Outlet Factory";
+    public HashMap<String, Integer> dragonPrice = new HashMap<>();
+    public ArrayList<Dragon> usedDragons = new ArrayList<>();
+    public ArrayList<Player> players = new ArrayList<>();
+    private int price;
+
 }

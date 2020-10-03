b51f68e81d9acb71c9f66ea23eee66e39eb292a5
autumnjava <creizy@gmail.com>
2020-10-03 10:47:28 +0200

    Starting to create in Game class fields

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,4 +1,15 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Game {
+    private int currentRound = 1;
+    private int maxRounds = 30;
+    private final int minRounds = 5;
+    private FactoryStore factoryStore = new FactoryStore();
+
+    private ArrayList<Player> players = new ArrayList<>();
+    private boolean isRunning = true;
+
+
 }

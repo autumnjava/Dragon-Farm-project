e1fa2b27298876d61d6cc27b9216df6a8a0d477f
autumnjava <creizy@gmail.com>
2020-10-03 10:54:04 +0200

    Starting to create fields in Dragon class

--- src/com/company/Dragon.java
+++ src/com/company/Dragon.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class Dragon {
+    static int [] prices = {4000, 5000, 6000, 7000, 8000, 9000};
+    static String [] acceptedFood = {"Meat, Fish", "Meat, Grass", "Grass, Metal",
+            "Fish", "Metal"};
+
+    public String name;
+    public String gender;
+    private int price;
+    private int healthPercent = 100;
 }

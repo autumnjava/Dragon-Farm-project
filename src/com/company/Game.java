cccd9730a8b034b6ef12de2e8d5dc3168ae9d863
autumnjava <creizy@gmail.com>
2020-10-03 11:24:46 +0200

    created a new field names [] which is a default names if player is lazy added method getPlayer to get current player

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -6,10 +6,31 @@ public class Game {
     private int currentRound = 1;
     private int maxRounds = 30;
     private final int minRounds = 5;
+    private int currentPlayer = 1;
     private FactoryStore factoryStore = new FactoryStore();
 
     private ArrayList<Player> players = new ArrayList<>();
     private boolean isRunning = true;
 
+    private String[] names = {"Player 1", "Player 2", "Player 3", "Player 4"}; // default names of players
+
+
+
+
+
+
+
+    public String getPlayer() {
+        if (currentPlayer == 1)
+            return names[0];
+        else if (currentPlayer == 2)
+            return names[1];
+        else if (currentPlayer == 3)
+            return names[2];
+        else if (currentPlayer == 4)
+            return names[3];
+        else
+            return null;
+    }
 
 }

12fcfe7a92598ff43f45c15b21d25601fe20eaef
autumnjava <creizy@gmail.com>
2020-10-03 14:46:20 +0200

    changed Menu.input to Menu.usersInput

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,18 +1,16 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
     Scanner scanner = new Scanner(System.in);
     public static int currentPlayer = 1;
     private FactoryStore factoryStore = new FactoryStore();
-
     private ArrayList<Player> players = new ArrayList<>();
 
 
-    public Game(String ...newNames) {
+    public Game() {
         Menu.print("\n".repeat(20) + "Welcome to Dragon Farm\n" + "-".repeat(30));
         Menu.print("created by Aleksandr S.\n2020 Malmö.\n" + "-".repeat(30));
         new Menu();
@@ -20,7 +18,7 @@ public class Game {
 
         System.out.println("See if we get here?");
         var names = Menu.getNames();
-        for(int i = 0; i < Menu.input; i++){
+        for(int i = 0; i < Menu.usersInput; i++){
             System.out.println(names[i]);
         }
 
@@ -34,7 +32,7 @@ public class Game {
             System.exit(0);
         }
         // create a new game
-        new Game(Menu.getNames());
+        new Game();
     }
 
     }

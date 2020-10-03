2e51c9b609a610e0aed8e9d1dcbfdfd548c00c4e
autumnjava <creizy@gmail.com>
2020-10-03 10:52:21 +0200

    Starting to create fields in Player class

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,12 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Player {
+    private String[] names = {"Player 1", "Player 2", "Player 3", "Player 4"}; // default names of players
+    private int moneyBalance = 15000; //start summa
+    private ArrayList<Dragon> dragonsOwned = new ArrayList<>();
+    private Game game;
+
+
 }

0a74d61bd731ff556e690285a61166f8209694da
autumnjava <creizy@gmail.com>
2020-10-03 14:29:13 +0200

    testing to print out user name list.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,13 +1,11 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
     Scanner scanner = new Scanner(System.in);
-    private int currentRound = 1;
-    private int maxRounds = 30;
-    private final int minRounds = 5;
     public static int currentPlayer = 1;
     private FactoryStore factoryStore = new FactoryStore();
 
@@ -18,15 +16,24 @@ public class Game {
         Menu.print("\n".repeat(20) + "Welcome to Dragon Farm\n" + "-".repeat(30));
         Menu.print("created by Aleksandr S.\n2020 Malmö.\n" + "-".repeat(30));
         new Menu();
+        int test = 0;
+
+        System.out.println("See if we get here?");
+        for(int i = 0; i < Menu.input; i++){
+            System.out.println(Arrays.toString(Menu.getNames()));
+        }
+
+        main(); //restart the game
 
     }
 
 
+    private void main() {
+        if(Menu.prompt("Play again? (y/n)?").equals("n")){
+            System.exit(0);
+        }
+        // create a new game
+        new Game(Menu.getNames());
+    }
 
-
-
-
-
-
-
-}
+    }

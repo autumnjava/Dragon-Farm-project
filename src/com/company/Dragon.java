7863cf2e7ee5d7370308a60df12940f73570e13a
autumnjava <creizy@gmail.com>
2020-10-04 15:24:04 +0200

    created constructor, added method "create" to create a new Dragon.

--- src/com/company/Dragon.java
+++ src/com/company/Dragon.java
@@ -1,12 +1,32 @@
 package com.company;
 
-public class Dragon {
-    static int [] prices = {4000, 5000, 6000, 7000, 8000, 9000};
-    static String [] acceptedFood = {"Meat, Fish", "Meat, Grass", "Grass, Metal",
+import java.util.Arrays;
+
+public abstract class Dragon {
+    static String[] dragonClasses = {"Lockheed", "Falkor", "Smaug", "Toothless", "Viserion"};
+    static int[] prices = {4000, 5000, 6000, 7000, 8000, 9000};
+    static String[] acceptedFood = {"Meat, Fish", "Meat, Grass", "Grass, Metal",
             "Fish", "Metal"};
 
-    public String name;
-    public String gender;
+    private String name;
+    private String gender;
     private int price;
     private int healthPercent = 100;
+
+    public Dragon(String name, String gender){
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public Dragon create(String name, String gender) {
+        var classIndex = Arrays.asList(dragonClasses).indexOf(this.getClass().getSimpleName());
+
+        if (!gender.equals("male") && !gender.equals("female")) {
+            throw new RuntimeException("Gender must be male or female!");
+        }
+        this.name = name;
+        this.gender = gender;
+        this.price = prices[classIndex];
+        return this;
+    }
 }

34f2ffda90cd2d6128de0580cd704b6d5b80ee48
autumnjava <creizy@gmail.com>
2020-10-04 11:37:10 +0200

    moved methods from Menu class to Game class as it was a bit too confusing for me.

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Menu {
     static Scanner scanner = new Scanner(System.in);
-    static int usersInput, roundsInput;
+
 
     private boolean isRunning = true;
     // default names of players
-    private static String[] names = {"Player 1", "Player 2", "Player 3", "Player 4"};
+
 
 
 
@@ -16,95 +16,14 @@ public class Menu {
         // start the game
         names = newNames.length < 4 ? names : newNames;
 
-        while (isRunning) {
             howManyUsers();
             howManyRounds();
-            isRunning = false;
-        }
 
     }
 
-    public static int howManyUsers(){
-        String error = "ONLY DIGITS [1-4]! Try again";
-            print("\n\nEnter how many users are going to play this game [1-4]");
-            try {
-                var userInput = scanner.nextLine();
-                usersInput = Integer.parseInt(userInput);
-                if (usersInput < 1 || usersInput > 4) {
-                    System.out.println(error);
-                    howManyUsers();
-                } else {
-                    print("You have entered " + usersInput);
-
-                    for(var i = 0; i < usersInput; i++){
-                        names[i] = Menu.prompt("Player " + (i + 1) + " name"
-                                + " (space + enter for \"" + names[i] + "\"):", names[i]);
-                    }
-                }
-
-            } catch (Exception e) {
-                print(error);
-                howManyUsers();
-            }
-        return usersInput;
-    }
-
-    private static int howManyRounds() {
-        String error = "ONLY DIGITS [5-30]! Try again";
-        try {
-                print("Enter how many rounds you want to play! [5-30].");
-                var userInput = scanner.nextLine();
-                roundsInput = Integer.parseInt(userInput);
-                if (roundsInput < 5 || roundsInput > 30) {
-                    System.out.println(error);
-                    howManyRounds();
-                } else {
-                    print("You have entered " + roundsInput);
-                }
-
-            } catch (Exception e) {
-                System.out.println(error);
-                howManyRounds();
-            }
-        return roundsInput;
-    }
-
-    public static void menuChoice(){
-        System.out.println("You will now get 5 menu choices:");
-        System.out.println("a. Buy dragons" +
-                "\nb. Buy food" +
-                "\nc. Feed dragons" +
-                "\nd. Pair dragons (50-50 chance to succeed)" +
-                "\ne. Sell dragons");
-        var input = scanner.nextLine();
-        switch (input) {
-            case "a" -> print("You decided to buy dragons!");
-            case "b" -> print("You decided to buy food");
-            case "c" -> print("You decided to feed your dragons");
-            case "d" -> print("You decided to pair dragons");
-            case "e" -> print("You decided to sell dragons");
-            default -> {
-                print("Wrong input!"); //start the method again
-                menuChoice();
-            }
-        }
-    }
 
 
-
-    static public void print(String x){
-        // print a string if it is not empty
-        if(!x.equals("")){ System.out.println(x); }
-    }
-
-    static public String prompt(String message, String ..._default){
-        // prompt the user for an answer - use default answer (if such) if none
-        print(message);
-        var answer = scanner.nextLine().trim(); // trim removes spaces at start & end
-        return answer.equals("") && _default.length > 0 ? _default[0] : answer;
-    }
-
-    public static String getPlayer() {
+/*    public static String getPlayer() {
         if (Game.currentPlayer == 1)
             return names[0];
         else if (Game.currentPlayer == 2)
@@ -115,12 +34,10 @@ public class Menu {
             return names[3];
         else
             return null;
-    }
+    }*/
 
 
-    public static String[] getNames(){
-        return names;
-    }
+
 
 
 }

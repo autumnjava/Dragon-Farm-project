1e21a25ffad167c475dfe6ea1b64d707e905739c
autumnjava <creizy@gmail.com>
2020-10-04 15:46:21 +0200

    trying to create dragons. created an Array of dragons. converted that to an ArrayList. then downcasting them correctly and calling method fly()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,31 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        new Game();
+        // write your code here
+        //new Game();
+
+        //testing to create new Dragons
+        Dragon[] temp = {
+                new Falkor("Fido", "male"),
+                new Falkor("Fido234", "male"),
+                new Falkor("Fido234", "male")
+        };
+        ArrayList<Dragon> dragons = new ArrayList(Arrays.asList(temp));
+
+        System.out.println("Asking dragons to fly");
+
+        for(var dragon: dragons){
+            var className = dragon.getClass().getSimpleName();
+            switch(className){
+                case "Falkor" -> ((Falkor) dragon).fly();
+                //case "Cat" -> ((Cat) animal).meow();
+            }
+        }
+
+
     }
 }

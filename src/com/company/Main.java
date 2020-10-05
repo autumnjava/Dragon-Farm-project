c0b9b4e07e29bbef531bf42555a2718c4263f780
autumnjava <creizy@gmail.com>
2020-10-05 14:17:54 +0200

    removed some test code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,26 +7,5 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         new Game();
-
-/*        //testing to create new Dragons
-        Dragon[] temp = {
-                new Falkor("Falkorito", "male"),
-                new Viserion("Ok", "female")
-        };
-        var dragons = new ArrayList(Arrays.asList(temp));
-
-        System.out.println("Asking dragons to fly");
-
-        for(var dragon: dragons){
-            var className = dragon.getClass().getSimpleName();
-            switch(className){
-                case "Falkor" -> ((Falkor) dragon).fly();
-                case "Viserion" -> ((Viserion) dragon).fly();
-            }
-        }*/
-
-
-
-
     }
 }

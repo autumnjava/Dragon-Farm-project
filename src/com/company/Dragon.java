37db5410a59960a17aff83fd17d3045d337e909f
autumnjava <creizy@gmail.com>
2020-10-06 10:07:46 +0200

    commented out some fields, created test method decreaseHealth, and created a Getter to get HealthPercent

--- src/com/company/Dragon.java
+++ src/com/company/Dragon.java
@@ -3,28 +3,34 @@ package com.company;
 import java.util.Arrays;
 
 public abstract class Dragon {
-    static String[] dragonClasses = {"Lockheed", "Falkor", "Smaug", "Toothless", "Viserion"};
-    static int[] prices = {4000, 5000, 6000, 7000, 8000, 9000};
+//    static String[] dragonClasses = {"Lockheed", "Falkor", "Smaug", "Toothless", "Viserion"};
+//    static int[] prices = {4000, 5000, 6000, 7000, 8000};
     static String[] acceptedFood = {"Meat, Fish", "Meat, Grass", "Grass, Metal",
             "Fish", "Metal"};
+
 
     public String name;
     private String gender;
     private int healthPercent = 100;
-    private int price;
+    //private int dragonPrice;
 
     public Dragon (String name, String gender) {
-        var classIndex = Arrays.asList(dragonClasses).indexOf(this.getClass().getSimpleName());
+//        var classIndex = Arrays.asList(dragonClasses).indexOf(this.getClass().getSimpleName());
 
-        if (!gender.equals("male") && !gender.equals("female")) {
-            throw new RuntimeException("Gender must be male or female!");
-        }
         this.name = name;
         this.gender = gender;
-        this.price = prices[classIndex];
+        //this.dragonPrice = prices[classIndex];
     }
 
     public String presentYourself(){
         return name;
+    }
+
+    public int decreaseHealth(){
+        return healthPercent-10;
+    }
+
+    public int getHealthPercent() {
+        return healthPercent;
     }
 }

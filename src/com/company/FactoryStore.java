5339e62b787e98fb5c7818b6822a8128349a7490
autumnjava <creizy@gmail.com>
2020-10-06 11:19:21 +0200

    created askName() and askGender() methods which return strings, used them to create new Objects.

--- src/com/company/FactoryStore.java
+++ src/com/company/FactoryStore.java
@@ -4,50 +4,65 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FactoryStore {
-    static private Player player;
-
-    public String nameOfStore = "Dragons and Food Outlet Factory";
-    public HashMap<String, Integer> dragonPrice = new HashMap<>();
+    public HashMap<Object, Integer> dragonPrice = new HashMap<>();
     public ArrayList<Dragon> usedDragons = new ArrayList<>();
     public ArrayList<Player> players = new ArrayList<>();
     private int price;
 
 
-
-    public static Dragon askAndCreateDragon() {
-        var input = Game.prompt("Choose a dragon:\na. Lockheed\nb. Falkor\nc. Smaug" +
+    public static Dragon askAndCreateDragon(Player player) {
+        var input = Game.prompt("Choose a dragon:\na. Lockheed price: \nb. Falkor\nc. Smaug" +
                 "\nd. Toothless\ne. Viserion");
         var inputChar = input.charAt(0);
         if (inputChar != 'a' && inputChar != 'b' && inputChar != 'c'
                 && inputChar != 'd' && inputChar != 'e') {
             System.out.println("ERROR! try again");
-            askAndCreateDragon();
+            askAndCreateDragon(player);
         } else {
-            var name = Game.prompt("Enter a name of a dragon:");
-            var gender = Game.prompt("Enter a gender [male/female]:");
-            if (!gender.equals("male") && !gender.equals("female")) {
-                System.out.println("Wrong input! Try again");
-                gender = Game.prompt("Enter a gender [male/female]:");
-            }
             switch (inputChar) {
                 case 'a' -> {
-                    return new Lockheed(name, gender);
+                    System.out.println("Ok so you want to buy a Lockheed");
+                    //check if player has enough cash
+//                    if(player.getMoneyBalance() < Dragon.p){
+//                        System.out.println("Not enough money!");
+//                    } else
+                    return new Lockheed(askName(), askGender());
                 }
                 case 'b' -> {
-                    return new Falkor(name, gender);
+                    return new Falkor(askName(), askGender());
                 }
                 case 'c' -> {
-                    return new Smaug(name, gender);
+                    return new Smaug(askName(), askGender());
                 }
                 case 'd' -> {
-                    return new Toothless(name, gender);
+                    return new Toothless(askName(), askGender());
                 }
                 case 'e' -> {
-                    return new Viserion(name, gender);
+                    return new Viserion(askName(), askGender());
                 }
             }
         }
         //Theoretically we should not get here.
         return null;
     }
+    public static String askName(){
+        return Game.prompt("Enter a name of a dragon:");
+    }
+
+    public static String askGender(){
+        var gender = Game.prompt("Enter a gender [male/female]:");
+        if (gender.equals("male")){
+            return "male";
+        }
+
+        else if(gender.equals("female")){
+            return "female";
+        }
+        else{
+            askGender();
+        }
+            return null; //in theory we shall not get here!
+        }
+
+
 }

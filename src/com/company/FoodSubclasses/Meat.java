525614c69d3ca29a36027e1772ad8ef83ebe6bdd
autumnjava <creizy@gmail.com>
2020-10-08 14:10:40 +0200

    moved to a new package FoodSubclasses

---
+++ src/com/company/FoodSubclasses/Meat.java
@@ -0,0 +1,6 @@
+package com.company.FoodSubclasses;
+
+import com.company.Food;
+
+public class Meat extends Food {
+}
---
+++ src/com/company/FoodSubclasses/Metal.java
@@ -0,0 +1,6 @@
+package com.company.FoodSubclasses;
+
+import com.company.Food;
+
+public class Metal extends Food {
+}

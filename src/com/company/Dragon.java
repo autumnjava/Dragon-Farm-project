a0e62ac68b038366597cb3b7e9a5c8976a27e939
autumnjava <creizy@gmail.com>
2020-10-08 14:15:46 +0200

    changed fields to protected. Removed getters. wrote a simple method addFood, which I am gonna call in each Dragon subclass to add values into ArrayList of Strings that each Dragon can eat (foodDragonCanEat)

--- src/com/company/Dragon.java
+++ src/com/company/Dragon.java
@@ -1,56 +1,36 @@
 package com.company;
 
-public abstract class Dragon {
-    private String name;
-    private String gender;
-    private int healthPercent;
-    private int dragonPrice;
-    private Player owner;
-    private int age;
-    private int maxAge;
-    private String foodDragonCanEat;
+import java.util.*;
 
-    public Dragon (String name, String gender, Player owner, int dragonPrice, int healthPercent, int age, int mageAxe, String foodDragonCanEat) {
+public abstract class Dragon {
+    protected String name;
+    protected String gender;
+    protected Player owner;
+
+    protected int healthPercent;
+    protected int dragonPrice;
+    protected int age;
+    protected int maxAge;
+    protected ArrayList<String> foodDragonCanEat;
+
+    public Dragon (String name, String gender, Player owner) {
         this.name = name;
         this.gender = gender;
         this.owner = owner;
-        this.dragonPrice = dragonPrice;
-        this.healthPercent = healthPercent;
-        this.age = age;
-        this.maxAge = mageAxe;
-        this.foodDragonCanEat = foodDragonCanEat;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public int getHealthPercent() {
-        return healthPercent;
-    }
-
-    public int getDragonPrice() {
-        return dragonPrice;
-    }
-
-    public Player getOwner() {
-        return owner;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public int getMaxAge() {
-        return maxAge;
     }
 
     public int decreaseHealth() {
         var randomNr = (int) ((Math.random() * (31 - 10)) + 10);
         return healthPercent -= randomNr;
     }
+
+    public ArrayList<String> addFood(String... args){
+        ArrayList<String> foodDragonCanEat = new ArrayList<>();
+        for(var x: args){
+            foodDragonCanEat.add(x);
+        }
+        return foodDragonCanEat;
+    }
+
+
 }

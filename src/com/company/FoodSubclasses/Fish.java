0a64b94de81a02ca5d7e4f6aa6b43306258af4ef
autumnjava <creizy@gmail.com>
2020-10-09 14:29:23 +0200

    created fields for Fish super class, created an empty constructor (to add different fish to the store). Created constuctor with parameter weight for player to buy/create desired amount of food

--- src/com/company/FoodSubclasses/Fish.java
+++ src/com/company/FoodSubclasses/Fish.java
@@ -3,4 +3,11 @@ package com.company.FoodSubclasses;
 import com.company.Food;
 
 public class Fish extends Food {
+    public Fish(){
+        super("fish", 200, 0);
+    }
+
+    public Fish(int weight){
+        super("fish", 200, weight);
+    }
 }

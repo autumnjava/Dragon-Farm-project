cb02b459cf1d0cd06d93520892e33d4fc7cfa78d
autumnjava <creizy@gmail.com>
2020-10-09 14:29:58 +0200

    created fields for Fish super class, created an empty constructor (to add different fish to the store). Created constuctor with parameter weight for player to buy/create desired amount of food

--- src/com/company/Food.java
+++ src/com/company/Food.java
@@ -1,7 +1,13 @@
 package com.company;
 
 public abstract class Food {
-    private String name;
-    private int price;
-    private int weight;
+    protected String name;
+    protected int price;
+    protected int weight;
+
+    public Food(String name, int price, int weight){
+        this.name = name;
+        this.price = price;
+        this.weight = weight;
+    }
 }

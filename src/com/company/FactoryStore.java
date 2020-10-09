2e4ad8d2b6fcfa2ef9b42adf9c8ccdc8f8ed5a7f
autumnjava <creizy@gmail.com>
2020-10-09 16:15:58 +0200

    created methods foodYouCanBuy (similar to dragonsYouCanBuy) and buyFood(): similar to buyDragon. plus some extra help methods: printFoodYouCanBuy, buyMoreFood, and setBalanceFood. will try later to group them together with other methods.

--- src/com/company/FactoryStore.java
+++ src/com/company/FactoryStore.java
@@ -34,7 +34,6 @@ public class FactoryStore {
         foodForSale.put("Grass", new Grass());
         foodForSale.put("Fish", new Fish());
         foodForSale.put("Meat", new Meat());
-        foodForSale.put("Metal", new Metal());
     }
 
     public ArrayList<String> dragonsYouCanBuy(){
@@ -45,7 +44,59 @@ public class FactoryStore {
                     dragonsAvailable.add(dragon);
                 }
             }
-return dragonsAvailable;
+            return dragonsAvailable;
+    }
+    public ArrayList<String> foodYouCanBuy(){
+        ArrayList<String> foodAvailable = new ArrayList<>();
+
+        for(var food: foodForSale.keySet()){
+            if(player.getMoneyBalance() >= foodForSale.get(food).price){
+                foodAvailable.add(food);
+            }
+        }
+        return foodAvailable;
+    }
+
+    public void buyFood(){
+        if(foodForSale.size() > 0 && player.getMoneyBalance() > 0){
+            printFoodYouCanBuy();
+            try{
+                System.out.println("NOTE: enter 999 to next player / next round");
+                var input = Game.prompt("Which one you want to buy? [1-"
+                        + foodYouCanBuy().size() + "]");
+                var inputInt = Integer.parseInt(input);
+                if(inputInt>dragonsYouCanBuy().size() && inputInt != 999){
+                    System.out.println("No cheating!");
+                    buyFood();
+                }
+                switch(inputInt){
+                    case 1 -> {
+                        player.foodOwned.add(new Grass(1));
+                        setBalanceFood("grass");
+                        buyMoreFood();
+                    }
+                    case 2 -> {
+                        player.foodOwned.add(new Fish(1));
+                        setBalanceFood("fish");
+                        buyMoreFood();
+                    }
+                    case 3 -> {
+                        player.foodOwned.add(new Meat(1));
+                        setBalanceFood("meat");
+                        buyMoreFood();
+                    }
+                    case 999 -> System.out.println("you decided to skip round/player");
+                }
+            } catch (Exception e) {
+                System.out.println("Only numbers!");
+                buyFood();
+            }
+        } else {
+            System.out.println("You cant buy anything, sorry!");
+            if(player.getMoneyBalance() > 0){
+                game.menuChoice(player);
+            }
+        }
     }
 
     public void buyDragons(){
@@ -111,10 +162,27 @@ return dragonsAvailable;
         }
     }
 
+    public void printFoodYouCanBuy(){
+        int counter = 1;
+        System.out.println("\n".repeat(20) + "Dragons you can buy:");
+        for(var food: foodYouCanBuy()){
+            System.out.println(counter + ". " + food + " at price " +
+                    foodForSale.get(food).price);
+            counter++;
+        }
+    }
+
     public void buyMore(){
         if(dragonsYouCanBuy().size() > 0) {
             System.out.println("\n".repeat(20) + "You can buy more dragons if you want!");
             buyDragons();
+        }
+    }
+
+    public void buyMoreFood(){
+        if(dragonsYouCanBuy().size() > 0) {
+            System.out.println("\n".repeat(20) + "You can buy more food if you want!");
+            buyFood();
         }
     }
 
@@ -142,5 +210,9 @@ return dragonsAvailable;
 
     public void setBalance(String dragon){
         player.setMoneyBalance(player.getMoneyBalance() - dragonsForSale.get(dragon).dragonPrice);
+    }
+
+    public void setBalanceFood(String food){
+        player.setMoneyBalance(player.getMoneyBalance() - foodForSale.get(food).price);
     }
 }

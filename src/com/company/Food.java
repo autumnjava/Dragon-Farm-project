f3559462dda296644d1fe88f22e5d12fc6c8f11f
autumnjava <creizy@gmail.com>
2020-10-15 10:47:48 +0200

    added field finalPrice and setter setWeight.

--- src/com/company/Food.java
+++ src/com/company/Food.java
@@ -4,10 +4,16 @@ public abstract class Food {
     protected String name;
     protected int price;
     protected int weight;
+    protected int finalPrice;
 
-    public Food(String name, int price, int weight){
+    public Food(String name, int price, int weight, int finalPrice){
         this.name = name;
         this.price = price;
+        this.weight = weight;
+        this.finalPrice = finalPrice;
+    }
+
+    public void setWeight(int weight) {
         this.weight = weight;
     }
 }
